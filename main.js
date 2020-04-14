7c13149fcdb78433804518ec9327c6e607a9459d
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-04-14 11:38:25 -0300

    feat: add application tray

--- main.js
+++ main.js
@@ -1,18 +1,67 @@
-const { BrowserWindow, app } = require('electron');
-
+const { BrowserWindow, app, Tray, Menu } = require('electron');
+const path = require('path');
 require('./bin/www');
+
+const applicationIcon = './build/icon.ico';
 
 let mainWindow = null;
 
-function main() {
+app.on('ready', () => {
   mainWindow = new BrowserWindow({
     resizable: true,
+    icon: applicationIcon
   });
+
+  const applicationTray = new Tray(applicationIcon);
+
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        mainWindow.show()
+      }
+    },
+    {
+      label: 'Control',
+      click: () => {
+        mainWindow.loadURL('http://localhost:3636/admin');
+      }
+    },
+    {
+      label: 'Deck',
+      click: () => {
+        mainWindow.loadURL('http://localhost:3636/deck');
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuiting = true
+        app.quit()
+      }
+    }
+  ]);
+
+  applicationTray.setContextMenu(contextMenu);
+
   mainWindow.removeMenu();
   mainWindow.loadURL('http://localhost:3636');
   mainWindow.on('close', event => {
     mainWindow = null;
   });
-}
 
-app.on('ready', main);
+  mainWindow.on('minimize', event => {
+    event.preventDefault()
+    mainWindow.hide()
+  });
+
+  applicationTray.on('double-click', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('show', () => {
+    try {
+      applicationTray.setHighlightMode('always')
+    } catch (error) {}
+  });
+});
