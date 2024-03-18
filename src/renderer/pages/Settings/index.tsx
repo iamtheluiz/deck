e6ddb626b2de36f86d0a1a8c18653131c0f2eeda
iamtheluiz <iamtheluiz.dev@gmail.com>
2024-03-18 12:39:46 -0300

    refactor: review Settings page and remove styled-components

--- src/renderer/pages/Settings/index.tsx
+++ src/renderer/pages/Settings/index.tsx
@@ -3,8 +3,6 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 import QRCode from 'qrcode.react';
 
-import { LocalStyle, Container, QRCodeContainer } from './styles';
-
 // eslint-disable-next-line react/function-component-definition
 const Settings: React.FC = () => {
   const history = useHistory();
@@ -15,17 +13,18 @@ const Settings: React.FC = () => {
   }
 
   return (
-    <>
-      <LocalStyle />
-      <Container>
-        <header>
-          <FiArrowLeft size={30} onClick={handleNavigateToHome} />
-        </header>
-        <QRCodeContainer>
-          <QRCode value={`http://${address}:4531`} renderAs="svg" />
-        </QRCodeContainer>
-      </Container>
-    </>
+    <div className="flex flex-col flex-1 p-2">
+      <header>
+        <FiArrowLeft size={16} onClick={handleNavigateToHome} />
+      </header>
+      <div className="flex justify-center items-center flex-1 w-full">
+        <QRCode
+          value={`http://${address}:4531`}
+          renderAs="svg"
+          className="w-64 h-64"
+        />
+      </div>
+    </div>
   );
 };
 
